ad624cadd1926146329100bb886b38d54335f2b2
ArianaMartinMartinez <arianamartinmartinez@gmail.com>
2025-01-03 00:47:14 +0100

    feat: add routes to index and show for Vehicles

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\VehicleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/vehicles', [VehicleController::class, 'index'])->name('apiHomeVehicles');
+Route::get('/vehicles/{id}', [VehicleController::class, 'show'])->name('apiShowVechicle');
