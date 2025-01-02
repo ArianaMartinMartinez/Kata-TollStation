42615a5138f17bcb3099b523a09c47bf8124e842
ArianaMartinMartinez <arianamartinmartinez@gmail.com>
2025-01-03 00:45:28 +0100

    feat: add index and show method for Vehicle

---
+++ app/Http/Controllers/Api/VehicleController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Vehicle;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class VehicleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $vehicles = Vehicle::with('type')->get();
+
+        return response()->json($vehicles, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $vehicle = Vehicle::with('type')->findOrFail($id);
+
+        return response()->json($vehicle, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
