9bc6fe994eff259b3a3bb310694f5d911126e105
ArianaMartinMartinez <arianamartinmartinez@gmail.com>
2025-01-03 00:55:18 +0100

    feat: add post, put and delete routes to Vehicles

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/vehicles', [VehicleController::class, 'index'])->name('apiHomeVehicles');
 Route::get('/vehicles/{id}', [VehicleController::class, 'show'])->name('apiShowVechicle');
+Route::post('/vehicles', [VehicleController::class, 'store'])->name('apiStoreVehicle');
+Route::put('/vehicles/{id}', [VehicleController::class, 'update'])->nameç('apiUpdateVehicles');
+Route::delete('/vehicles/{id}', [VehicleController::class, 'destroy'])->name('apiDestroyVehicle');
