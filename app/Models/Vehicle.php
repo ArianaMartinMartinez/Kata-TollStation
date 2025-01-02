177cbc0bd16915aac61d8590108a79ab1a5aaf6c
ArianaMartinMartinez <arianamartinmartinez@gmail.com>
2025-01-02 19:39:18 +0100

    feat: add Vehicle model

---
+++ app/Models/Vehicle.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Vehicle extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'license',
+        'axles',
+        'id_type',
+    ];
+
+    public function type(): BelongsTo {
+        return $this->belongsTo(Type::class);
+    }
+
+    public function tolls(): HasMany {
+        return $this->hasMany(Stations_Vehicles::class);
+    }
+}
