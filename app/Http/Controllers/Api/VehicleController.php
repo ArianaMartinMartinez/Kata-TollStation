1c351734d7e16f9a834c9f6c3dd0176d68642e6b
ArianaMartinMartinez <arianamartinmartinez@gmail.com>
2025-01-03 19:47:27 +0100

    feat: add post method for Vehicles

--- app/Http/Controllers/Api/VehicleController.php
+++ app/Http/Controllers/Api/VehicleController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Models\Vehicle;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Type;
+use Illuminate\Support\Facades\Validator;
 
 class VehicleController extends Controller
 {
@@ -23,7 +25,38 @@ class VehicleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'license' => 'required',
+            'axles' => 'nullable | integer | between:1,3',
+            'type' => 'required | exists:types,type',
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'message' => 'Los datos introducidos no son correctos',
+                'errors' => $validator->errors(),
+            ], 400);
+        }
+
+        $validated = $validator->validate();
+
+        $vehicleType = Type::where('type', $validated['type'])->first();
+
+        if($vehicleType->type == 'truck' && empty($validated['axles'])) {
+            return response()->json([
+                'message' => 'Number of axles is required for trucks',
+            ], 400);
+        }
+
+        $vehicle = Vehicle::create([
+            'license' => $validated['license'],
+            'id_type' => $vehicleType->id,
+            'axles' => $vehicleType->type === 'truck' ? $validated['axles'] : null,
+        ]);
+
+        $vehicle->save();
+
+        return response()->json($vehicle, 201);
     }
 
     /**
