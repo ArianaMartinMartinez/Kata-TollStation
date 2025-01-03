820c75953be9a0b776cf4ef8e4a71e5c261e8ab4
ArianaMartinMartinez <arianamartinmartinez@gmail.com>
2025-01-03 20:14:44 +0100

    fix: fix route store vehicle name

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/vehicles', [VehicleController::class, 'index'])->name('apiHomeVehicles');
 Route::get('/vehicles/{id}', [VehicleController::class, 'show'])->name('apiShowVechicle');
 Route::post('/vehicles', [VehicleController::class, 'store'])->name('apiStoreVehicle');
-Route::put('/vehicles/{id}', [VehicleController::class, 'update'])->nameç('apiUpdateVehicles');
+Route::put('/vehicles/{id}', [VehicleController::class, 'update'])->name('apiUpdateVehicles');
 Route::delete('/vehicles/{id}', [VehicleController::class, 'destroy'])->name('apiDestroyVehicle');
