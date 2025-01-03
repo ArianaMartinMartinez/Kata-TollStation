2361f5ab6ebd0c6c2cd52f42a7d466d73466530e
ArianaMartinMartinez <arianamartinmartinez@gmail.com>
2025-01-03 23:23:37 +0100

    feat: add index and show methods for Station

---
+++ app/Http/Controllers/Api/StationController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Station;
+use Illuminate\Http\Request;
+
+class StationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $stations = Station::all();
+
+        return response()->json($stations, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $station = Station::findOrFail($id);
+
+        return response()->json($station, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
