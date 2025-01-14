544009ca549ab5ac356c9077c2c41e83622bb941
Ariana <104553851+ArianaMartinMartinez@users.noreply.github.com>
2025-01-14 21:35:05 +0100

    feat: add update method in StationVehicleController

--- app/Http/Controllers/Api/StationVehicleController.php
+++ app/Http/Controllers/Api/StationVehicleController.php
@@ -76,7 +76,42 @@ class StationVehicleController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $toll = Stations_Vehicles::findOrFail($id);
+
+        $validator = Validator::make($request->all(), [
+            'id_station' => 'required | exists:stations,id',
+            'id_vehicle' => 'required | exists:vehicles,id',
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'message' => 'Introduced data is not correct',
+                'errors' => $validator->errors(),
+            ], 400);
+        }
+
+        $validated = $validator->validate();
+
+        $vehicle = Vehicle::with('type')->findOrFail($validated['id_vehicle']);
+
+        $amount = 0;
+        if($vehicle->type->type === 'truck') {
+            $amount = $vehicle->axles * $vehicle->type->price;
+        } else {
+            $amount = $vehicle->type->price;
+        }
+
+        $toll->update([
+            'id_station' => $validated['id_station'],
+            'id_vehicle' => $validated['id_vehicle'],
+            'amount' => $amount,
+        ]);
+
+        $toll->save();
+
+        $fullToll = Stations_Vehicles::with('station', 'vehicle')->findOrFail($toll->id);
+
+        return response()->json($fullToll, 200);
     }
 
     /**
