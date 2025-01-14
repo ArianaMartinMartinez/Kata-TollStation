8dcd8676702f236652b983bb862455355c09fc21
Ariana <104553851+ArianaMartinMartinez@users.noreply.github.com>
2025-01-14 21:25:33 +0100

    feat: store and destroy methods in StationVehicleController

--- app/Http/Controllers/Api/StationVehicleController.php
+++ app/Http/Controllers/Api/StationVehicleController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Stations_Vehicles;
+use App\Models\Vehicle;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class StationVehicleController extends Controller
 {
@@ -23,7 +25,40 @@ class StationVehicleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'id_station' => 'required | exists:stations,id',
+            'id_vehicle' => 'required | exists:vehicles,id',
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'message' => 'Introduced data is not correct',
+                'errors' => $validator->errors(),
+            ], 400);
+        }
+
+        $validated = $validator->validate();
+
+        $vehicle = Vehicle::with('type')->findOrFail($validated['id_vehicle']);
+
+        $amount = 0;
+        if($vehicle->type->type === 'truck') {
+            $amount = $vehicle->axles * $vehicle->type->price;
+        } else {
+            $amount = $vehicle->type->price;
+        }
+
+        $toll = Stations_Vehicles::create([
+            'id_station' => $validated['id_station'],
+            'id_vehicle' => $validated['id_vehicle'],
+            'amount' => $amount,
+        ]);
+
+        $toll->save();
+
+        $fullToll = Stations_Vehicles::with('station', 'vehicle')->findOrFail($toll->id);
+
+        return response()->json($fullToll, 201);
     }
 
     /**
@@ -49,6 +84,11 @@ class StationVehicleController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $toll = Stations_Vehicles::findOrFail($id);
+        $toll->delete();
+
+        return response()->json([
+            'message' => 'Toll deleted',
+        ], 200);
     }
 }
