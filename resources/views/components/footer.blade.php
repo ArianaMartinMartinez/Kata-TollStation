3a7d8952f5ed4d210b3ca6b42e92b8b42513c74f
Ariana <104553851+ArianaMartinMartinez@users.noreply.github.com>
2025-01-17 17:47:00 +0100

    feat: add layout, header and footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,14 @@
+<footer>
+    <div class="socials">
+        <a href="mailto:arianamartinmartinez@gmail.com" target="_blank">
+            <img src="{{ asset('img/socials/gmailLogo.png') }}" alt="Gmail Logo" width="50px">
+        </a>
+        <a href="https://github.com/ArianaMartinMartinez" target="_blank">
+            <img src="{{ asset('img/socials/githubLogo.png') }}" alt="GitHub Logo" width="50px">
+        </a>
+        <a href="https://www.linkedin.com/in/ariana-martin-martinez/" target="_blank">
+            <img src="{{ asset('img/socials/linkedinLogo.png') }}" alt="Linkedin Logo" width="50px">
+        </a>
+    </div>
+    <p>©Copyright 2025. All right reserved for Ariana Martín Martínez</p>
+</footer>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="logoDiv">
+        <a href="{{ route('home') }}">
+            <img src="{{ asset('img/icon.png') }}" alt="Toll Stations App logo">
+            <h1>Toll Stations App</h1>
+        </a>
+    </div>
+
+    <nav>
+        <ul>
+            <li><a href="">Stations</a></li>
+            <li><a href="">Vehicles</a></li>
+        </ul>
+    </nav>
+</header>
